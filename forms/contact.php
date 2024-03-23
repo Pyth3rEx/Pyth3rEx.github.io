c706ac6ceb555dfe9889d258e8a2169e3ff7567f
Pyth3rEx <pyth3rex@outlook.com>
2024-03-23 18:40:37 +0100

    updated form

--- forms/contact.php
+++ forms/contact.php
@@ -1,62 +1,73 @@
 <?php
-// Only process POST reqeusts.
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the form fields and remove whitespace.
-    $name = strip_tags(trim($_POST["name"]));
-    $name = str_replace(array("\r","\n"),array(" "," "),$name);
-    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-    $subject = trim($_POST["subject"]);
-    $message = trim($_POST["message"]);
+require 'vendor/autoload.php';
 
-    // reCaptcha services
-    $recaptchaSecret = '6LcrbaIpAAAAACNRNUYeD7JcPXlfCCM6F3Wjdh-Q';
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
-    $recaptcha = new \ReCaptcha\ReCaptcha($recaptchaSecret);
-    $recaptchaResult = $recaptcha->verify($recaptchaResponse);
+// Inclure les dépendances Google Cloud à l'aide de Composer
+use Google\Cloud\RecaptchaEnterprise\V1\RecaptchaEnterpriseServiceClient;
+use Google\Cloud\RecaptchaEnterprise\V1\Event;
+use Google\Cloud\RecaptchaEnterprise\V1\Assessment;
+use Google\Cloud\RecaptchaEnterprise\V1\TokenProperties\InvalidReason;
 
-    // Check that data was sent to the mailer.
-    if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Set a 400 (bad request) response code and exit.
-        http_response_code(400);
-        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
-        exit;
+/**
+  * Créez une évaluation pour analyser le risque d'une action dans l'interface utilisateur.
+  * @param string $recaptchaKey La clé reCAPTCHA associée au site ou à l'application
+  * @param string $token Jeton généré auprès du client.
+  * @param string $project L'ID de votre projet Google Cloud.
+  * @param string $action Nom d'action correspondant au jeton.
+  */
+function create_assessment(
+  string $recaptchaKey,
+  string $token,
+  string $project,
+  string $action
+): void {
+  // Créez le client reCAPTCHA.
+  // À FAIRE : mettre en cache le code de génération du client (recommandé) ou appeler client.close() avant de quitter la méthode.
+  $client = new RecaptchaEnterpriseServiceClient();
+  $projectName = $client->projectName($project);
+
+  // Définissez les propriétés de l'événement à suivre.
+  $event = (new Event())
+    ->setSiteKey($recaptchaKey)
+    ->setToken($token);
+
+  // Créez la demande d'évaluation.
+  $assessment = (new Assessment())
+    ->setEvent($event);
+
+  try {
+    $response = $client->createAssessment(
+      $projectName,
+      $assessment
+    );
+
+    // Vérifiez si le jeton est valide.
+    if ($response->getTokenProperties()->getValid() == false) {
+      printf('The CreateAssessment() call failed because the token was invalid for the following reason: ');
+      printf(InvalidReason::name($response->getTokenProperties()->getInvalidReason()));
+      return;
     }
 
-    // Set the recipient email address.
-    $recipient = "hireme@pyth3rex.com";
-
-    // Set the email subject.
-    $subject = "New contact from $name";
-
-    // Build the email content.
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Subject: $subject\n";
-    $email_content .= "Message:\n$message\n";
-
-    // Build the email headers.
-    $email_headers = "From: $name <$email>";
-
-    if (!$recaptchaResult->isSuccess()) {
-      // reCaptcha failed, set a 403 (forbidden) response code.
-      http_response_code(403);
-      echo "Oops! reCaptcha didn't let us send the message. Did you complete the challenge?";
+    // Vérifiez si l'action attendue a été exécutée.
+    if ($response->getTokenProperties()->getAction() == $action) {
+      // Obtenez le score de risques et le ou les motifs.
+      // Pour savoir comment interpréter l'évaluation, consultez les pages suivantes :
+      // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+      printf('The score for the protection action is:');
+      printf($response->getRiskAnalysis()->getScore());
     } else {
-      // Send the email.
-      if (mail($recipient, $subject, $email_content, $email_headers)) {
-          // Set a 200 (okay) response code.
-          http_response_code(200);
-          echo "Thank You! Your message has been sent.";
-      } else {
-          // Set a 500 (internal server error) response code.
-          http_response_code(500);
-          echo "Oops! Something went wrong and we couldn't send your message.";
-      }
+      printf('The action attribute in your reCAPTCHA tag does not match the action you are expecting to score');
     }
-
-} else {
-    // Not a POST request, set a 403 (forbidden) response code.
-    http_response_code(403);
-    echo "There was a problem with your submission, please try again.";
+  } catch (exception $e) {
+    printf('CreateAssessment() call failed with the following error: ');
+    printf($e);
+  }
 }
+
+// À FAIRE : remplacer le jeton et les variables d'action reCAPTCHA avant d'exécuter l'exemple.
+create_assessment(
+   '6LcrbaIpAAAAAE7PpauMQbcYHmjQoRoMJEvVelX1',
+   'YOUR_USER_RESPONSE_TOKEN',
+   'pyth3rex-webpage-1516802293270',
+   'YOUR_RECAPTCHA_ACTION'
+);
 ?>
